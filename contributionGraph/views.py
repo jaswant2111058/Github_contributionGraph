9b9067f2b246b040d444aa89365f4d6bd2168f04
jaswant2111058 <jaswant2111058@akgec.ac.in>
2023-12-26 02:06:10 +0530

    deploy ready and fixing bugs

--- contributionGraph/views.py
+++ contributionGraph/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
-import json
 import requests
 from django.http import JsonResponse
-from django.conf import settings
-
-
 
 def home(request):
     # Retrieve the current working URL
     current_url = request.build_absolute_uri()
     return render(request, 'pages/home.html', {'current_url': current_url})
+
 def github_contributions(request):
     try:
-        
-        github_token = "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"
+        github_token = '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'
         username = request.GET.get('username')
 
         if not username:
@@ -40,21 +36,22 @@ def github_contributions(request):
         api_url = 'https://api.github.com/graphql'
         headers = {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer %s' % github_token
+            'Authorization': f'Bearer {github_token}'
         }
 
         # Send POST request to GitHub GraphQL API
         response = requests.post(api_url, headers=headers, json={'query': query})
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
 
-        if response.status_code == 200:
-            data = response.json()
-            contribution_data = data.get('data', {}).get('user', {}).get('contributionsCollection', {}).get('contributionCalendar', {})
-            return render(request, 'pages/graph.html', {'contribution_data': contribution_data,'username':username})
-        else:
-            error_message = data.get('message', 'Failed to fetch GitHub data')
-            raise ValueError(f"GitHub API error: {error_message}")
+        data = response.json()
+        contribution_data = data.get('data', {}).get('user', {}).get('contributionsCollection', {}).get('contributionCalendar', {})
+
+        return render(request, 'pages/graph.html', {'contribution_data': contribution_data, 'username': username})
+
+    except requests.RequestException as req_error:
+        return JsonResponse({'error': f'Request error: {str(req_error)}'}, status=500)
 
     except Exception as e:
         # Handle any unexpected exception
         error_message = str(e)
-        return JsonResponse({'error': "No username Exist"}, status=500)
+        return JsonResponse({'error': error_message}, status=500)
